1fd12a735453db19d42fc2403438ac159d0d22bc
Quontron <rquon.student@outlook.com>
2021-07-08 23:45:54 -0700

    Had issues with routing the backend properly to the products data.

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -16,15 +16,13 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-
         <Card.Text as='div'>
           <Rating
             value={product.rating}
             text={`${product.numReviews} reviews`}
           />
         </Card.Text>
-
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        ;<Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
